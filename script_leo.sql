1cbf79007d55a002984a95836f2a550ff4149b91
@yuri_assuncx <killerkoll2012@gmail.com>
2023-06-02 19:45:42 -0300

    fix: "sql updated"

--- script_leo.sql
+++ script_leo.sql
@@ -23,19 +23,19 @@ CREATE TABLE `barber` (
     `name` VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE `agendamento` (
+CREATE TABLE `schedule` (
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
     `user_id` INT NOT NULL,
     `barber_id` INT NOT NULL,
-    `servico` VARCHAR(50) NOT NULL,
-    `data_agend` VARCHAR(50) NOT NULL,
-    `hora_agend` VARCHAR(50) NOT NULL,
-    `observacao` VARCHAR(150),
-    `preco` INT NOT NULL,
+    `service` VARCHAR(50) NOT NULL,
+    `scheduled_data` VARCHAR(50) NOT NULL,
+    `scheduled_hour` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(150),
+    `price` INT NOT NULL,
     `status` BOOLEAN NOT NULL DEFAULT FALSE,
-    `data_criacao` TIMESTAMP DEFAULT NOW(),
+    `created_at` TIMESTAMP DEFAULT NOW(),
     
-    UNIQUE (`data_agend`, `hora_agend`, `barber_id`),
+    UNIQUE (`scheduled_data`, `scheduled_hour`, `barber_id`),
     FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE CASCADE,
     FOREIGN KEY (`barber_id`) REFERENCES `barber`(`id`) ON UPDATE CASCADE
 );
@@ -51,8 +51,8 @@ CREATE TABLE `feedback` (
     `barber_id` INT NOT NULL,
 	`user_id` INT NOT NULL,
     `is_liked` BOOLEAN NOT NULL,
-    `avaliacao` VARCHAR(35) NOT NULL,
-    `data_criacao` TIMESTAMP DEFAULT NOW(),
+    `description` VARCHAR(35) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT NOW(),
     
     FOREIGN KEY (`barber_id`) REFERENCES `barber`(`id`),
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
@@ -65,7 +65,9 @@ INSERT INTO `user`(`name`, `email`, `password`) VALUES ("teste2", "teste2", "tes
 
 INSERT INTO `barber`(`name`) VALUES ("barbeiro1");
 INSERT INTO `barber`(`name`) VALUES ("barbeiro2");
+INSERT INTO `barber`(`name`) VALUES ("barbeiro3");
 
-INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `avaliacao`) VALUES (3, 1, true, "Habilidade");
-INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `avaliacao`) VALUES (2, 1, true, "Gentileza");
-INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `avaliacao`) VALUES (1, 2, false, "Não gostei");
+INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `description`) VALUES (3, 1, true, "Habilidade");
+INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `description`) VALUES (3, 2, true, "Habilidade");
+INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `description`) VALUES (2, 1, true, "Gentileza");
+INSERT INTO `feedback`(`barber_id`, `user_id`, `is_liked`, `description`) VALUES (1, 2, false, "Não gostei");
